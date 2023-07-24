2aa513631b65c91c449c0221f122265cb43d9f40
shiiku419 <shiiku_419@softbank.ne.jp>
2023-07-24 13:21:23 +0900

    header

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
     def index
-      @event_lists = Post.order(:update_date).limit(5)
+      @event_lists = Post.order(updated_at: :desc).limit(5)
       @maps = Post.order(:id)
       @categories = Post.group(:category).count
     end
